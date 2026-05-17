064276e601c2397509c44973de96f85c2d9c84fd
JuhRBaptista <juliabapptista@gmail.com>
2026-05-17 15:24:55 +0100

    Issues ekf update

--- matlab/EKF/ekfUpdate.m
+++ matlab/EKF/ekfUpdate.m
@@ -1,25 +1,25 @@
-function [p, Cp] = ekfUpdate(p, Cp, V, G, R_mat)
+function [p, Cp] = ekfUpdate(p, Cp, V, G, R)
 
     if isempty(V)
         return;
     end
+    
+    R_mat = diag(R);
 
     % Innovation covariance
     S = G * Cp * G' + R_mat;
 
     % Kalman gain
-    K = Cp * G' / S;
+    K = Cp * G' *inv(S);
 
     % State update
     p = p + K * V;
-
-    % Normalize heading
     p(3) = normalizeAngle(p(3));
 
     % Covariance update
     I = eye(size(Cp));
 
     % Joseph stabilized form
-    Cp = (I - K * G) * Cp * (I - K * G)' + K * R_mat * K';
+    Cp = (I - K*G) * Cp;
 
 end
